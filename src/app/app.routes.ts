ec487c3023d0c036a24c56cb88ee9bfb70088f6f
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-04 14:51:26 +0600

    feat: app initiated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "login"
+    },
+    {
+        path: "login",
+        loadComponent: () => {
+            return import('./components/login/login.component')
+                .then((m) => m.LoginComponent)
+        },
+    },
+    {
+        path: "verify-login",
+        loadComponent: () => {
+            return import('./components/verify-login/verify-login.component')
+                .then((m) => m.VerifyLoginComponent)
+        },
+    },
+    {
+        path: "chat-list",
+        loadComponent: () => {
+            return import('./components/chat-list/chat-list.component')
+                .then((m) => m.ChatListComponent)
+        },
+    },
+];
