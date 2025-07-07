1e3335b79fc24ec32b1a595edb2b4c99e6c7be28
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-08 01:48:37 +0600

    refactor

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,59 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { ToastComponent } from '../toast/toast.component';
-import { ToastService } from '../../services/toast.service';
 import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  loginForm!: FormGroup;
+  email: string = "";
+  errorMessage = signal("");
   isLoading = signal(false);
 
   private authService = inject(AuthService);
-  private toastService = inject(ToastService);
   private router = inject(Router);
 
-  ngOnInit() {
-    this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email])
-    });
-  }
-
   onSubmit() {
-    if (this.loginForm.valid) {
-      const email = this.loginForm.value.email;
-      this.isLoading.set(true);
 
-      this.authService.login(email).subscribe({
-        next: (response) => {
-          this.authService.setEmail(email);
-          this.toastService.show('OTP Sent', 'success', 5000);
-          this.router.navigate(['/verify-login']);
-        },
-        error: (error) => {
-          this.isLoading.set(false);
-          const errorMessage = error?.error?.message || 'OTP Send Failed';
-          this.toastService.show(errorMessage, 'error', 5000);
-        },
-        complete: () => {
-          this.isLoading.set(false);
-        }
-      });
-    } else {
-      this.loginForm.markAllAsTouched();
+    if (!this.email) {
+      this.errorMessage.set("Email is empty");
+      return;
     }
-  }
 
-  get emailControl() {
-    return this.loginForm.get('email');
+    this.isLoading.set(true);
+
+    this.authService.login(this.email).subscribe({
+      next: (response) => {
+        this.authService.setEmail(this.email);
+        this.router.navigate(['/verify-login']);
+      },
+      error: (error) => {
+        this.isLoading.set(false);
+        this.errorMessage.set(error?.error?.message || 'OTP Send Failed');
+      },
+      complete: () => {
+        this.isLoading.set(false);
+      }
+    });
+
   }
 }
--- src/app/components/verify-login/verify-login.component.ts
+++ src/app/components/verify-login/verify-login.component.ts
@@ -1,87 +1,69 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { ToastService } from '../../services/toast.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verify-login',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './verify-login.component.html',
   styleUrl: './verify-login.component.css'
 })
 export class VerifyLoginComponent {
-  verifyLoginForm!: FormGroup;
+  otp: string = "";
   isLoading = signal(false);
   isResendLoading = signal(false);
+  errorMessage = signal("");
   email = signal<string | null>(null);
 
   private authService = inject(AuthService);
-  private toastService = inject(ToastService);
   private router = inject(Router);
 
   constructor() {
     this.email.set(this.authService.getEmail());
   }
 
-  ngOnInit() {
-    this.verifyLoginForm = new FormGroup({
-      otp: new FormControl('', [
-        Validators.required
-      ])
-    });
-  }
+  verifyOtp() {
 
-  onSubmit() {
-    if (this.verifyLoginForm.valid) {
-      const otp = this.verifyLoginForm.value.otp;
-      this.isLoading.set(true);
-
-      this.authService.verifyOtp(this.email(), otp).subscribe({
-        next: (response) => {
-          this.authService.clearEmail();
-          this.toastService.show('Verified', 'success', 5000);
-          this.router.navigate(['/chat-list']);
-        },
-        error: (error) => {
-          this.isLoading.set(false);
-          const errorMessage = error?.error?.message || 'OTP Verification Failed';
-          this.toastService.show(errorMessage, 'error', 5000);
-        },
-        complete: () => {
-          this.isLoading.set(false);
-        }
-      });
-
-    } else {
-      this.verifyLoginForm.markAllAsTouched();
+    if (!this.otp) {
+      this.errorMessage.set("OTP is empty");
+      return;
     }
+
+    this.isLoading.set(true);
+
+    this.authService.verifyOtp(this.email(), this.otp).subscribe({
+      next: (response) => {
+        this.authService.clearEmail();
+        this.router.navigate(['/chat-list']);
+      },
+      error: (error) => {
+        this.errorMessage.set(error?.error?.message || 'OTP Verification Failed');
+      },
+      complete: () => {
+        this.isLoading.set(false);
+      }
+    });
+
   }
 
   resendOtp() {
-    const savedEmail = this.email();
-    if (savedEmail) {
-      this.isResendLoading.set(true);
-      this.authService.login(savedEmail).subscribe({
-        next: (response) => {
-          this.toastService.show('OTP Resent', 'success', 5000);
-        },
-        error: (error) => {
-          const errorMessage = error?.error?.message || 'Failed to resend OTP';
-          this.toastService.show(errorMessage, 'error', 5000);
-        },
-        complete: () => {
-          this.isResendLoading.set(false);
-        }
-      });
-    } else {
-      this.toastService.show('No email found', 'error', 5000);
+    if (!this.email()) {
       this.router.navigate(['/login']);
+      return;
     }
-  }
 
-  get otpControl() {
-    return this.verifyLoginForm.get('otp');
+    this.isResendLoading.set(true);
+    this.authService.login(this?.email() || "").subscribe({
+      next: (response) => {
+      },
+      error: (error) => {
+        this.errorMessage.set(error?.error?.message || 'OTP Resend Failed');
+      },
+      complete: () => {
+        this.isResendLoading.set(false);
+      }
+    });
   }
 }
