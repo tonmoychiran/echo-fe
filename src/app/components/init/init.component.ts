9c7af7f2b1b7769471345ac7517d50fffc60b393
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-10 02:38:38 +0600

    feat: init component added

---
+++ src/app/components/init/init.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-init',
+  imports: [],
+  templateUrl: './init.component.html',
+  styleUrl: './init.component.css'
+})
+export class InitComponent {
+
+}
