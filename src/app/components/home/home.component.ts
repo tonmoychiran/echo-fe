f7871c4f8ee3ebcb7c578f40899f1da760608c65
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-11 13:33:30 +0600

    fix: selected anchor tag bg color not changing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ChatListComponent } from "../chat-list/chat-list.component";
 import { UserSectionComponent } from "../user-section/user-section.component";
-import { FriendsComponent } from "../friends/friends.component";
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ChatListComponent, UserSectionComponent, FriendsComponent],
+  imports: [ChatListComponent, UserSectionComponent, RouterLink, RouterOutlet, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
 }
