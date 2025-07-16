c0bb357b352f6b34dc445e3d39560d57f140c50a
tonmoychiranflyfartech <chiran@flyfar.tech>
2025-07-16 12:53:20 +0600

    refactor: chat list navigation fixed

--- src/app/components/chat-list/chat-list.component.ts
+++ src/app/components/chat-list/chat-list.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { AppRoutes } from '../../app.routes';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterOutlet, RouterModule],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.css'
 })
 export class ChatListComponent {
-
+  chatRoute=AppRoutes.CHAT;
 }
